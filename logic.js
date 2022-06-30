0e06dfd6d89495b97be01773040d546416cfd4f6
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2022-06-30 10:27:37 -0500

    colors added

--- logic.js
+++ logic.js
@@ -18,25 +18,34 @@ function createFeatures(earthquakeData) {
 
   function onEachFeature(feature, layer) {
 
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr>Magnitude: ${feature.properties.mag}`)
+    layer.bindPopup(`<h3>Depth ${feature.geometry.coordinates[2]} KM</h3><hr>Magnitude: ${feature.properties.mag}`)
 
   }
   function changeColor(feature) {
-    if (feature.geometry.coordinates[2] > 2)
-    return "black"
-    else if (feature.geometry.coordinates[2] > 1)
-    return "orange"
+    if (feature.geometry.coordinates[2] >= -10 && feature.geometry.coordinates[2]  <= 10 )
+    return "rgba(33, 244, 8, 0.8)"
+    else if (feature.geometry.coordinates[2] >= 10 && feature.geometry.coordinates[2]<= 30)
+    return "rgba(151, 244, 8, 0.8)"
+    else if (feature.geometry.coordinates[2] >= 30 && feature.geometry.coordinates[2]<= 50)
+    return "rgba(214, 244, 8, 0.8)"
+    else if (feature.geometry.coordinates[2] >= 50 && feature.geometry.coordinates[2]<= 70)
+    return "rgba(244, 185, 8, 0.8)"
+    else if (feature.geometry.coordinates[2] >= 70 && feature.geometry.coordinates[2]<= 90)
+    return "rgba(244, 131, 8, 0.8)"
+    else if (feature.geometry.coordinates[2] >= 70 && feature.geometry.coordinates[2]<= 90)
+    return "rgba(244, 82, 8, 0.8)"
     else
-    return "red"
+    return "rgba(244, 8, 8, 0.8)"
   }
   function geojsonMarkerOptions(feature) {
      return { 
-      radius: feature.properties.mag,
+      radius: feature.properties.mag * 4,
       fillColor: changeColor(feature),
-      color: "#000",
+      color: "",
       weight: 1,
-      opacity: 1,
-      fillOpacity: 0.8
+      opacity: 2,
+      fillOpacity: 0.8,
+      ratio: 1000
       
   }
 }
@@ -48,27 +57,37 @@ function createFeatures(earthquakeData) {
   var earthquakes = L.geoJSON(earthquakeData, {
     style: geojsonMarkerOptions,
 
-     
-     
-    
-      
-     
-    pointToLayer: function (feature, latlng) {
+      pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions)
         
     
       },
       
       onEachFeature: onEachFeature
-       
+      
 
   })
-
-
   
   createMap(earthquakes);
+// var legend = L.control({position: 'bottomright'});
 
- 
+// legend.onAdd = function (map) {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//     grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+//     labels = [];
+
+//     // loop through our density intervals and generate a label with a colored square for each interval
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+// }
+
+// return div;
+
+// };
+// legend.addTo(myMap); 
 }
 
 
@@ -106,54 +125,11 @@ function createMap(earthquakes) {
     zoom: 5,
     layers: [street, earthquakes]
   });
-
+  
   // Create a layer control that contains our baseMaps.
   // Be sure to add an overlay Layer that contains the earthquake GeoJSON.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(myMap);
-
-  // for (var i = 0; i < cities.length; i++) {
-  
-  //   L.circle(cities[i].geometry, {
-  //     fillOpacity: 0.75,
-  //     color: "white",
-  //     fillColor: "purple",
-  //     // Setting our circle's radius to equal the output of our markerSize() function:
-  //     // This will make our marker's size proportionate to its population.
-  //     radius: markerSize(cities[i].population)
-    
-    
-  //   }).bindPopup(`<h1>${cities[i].name}</h1> <hr> <h3>Population: ${cities[i].population.toLocaleString()}</h3>`).addTo(myMap);
-  // }
-  
-
+  }).addTo(myMap);  
 }
 
-// {"generated":1656344184000,
-// "url":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
-// "title":"USGS All Earthquakes, Past Week",
-// "status":200,
-// "api":"1.10.3",
-// "count":2130},
-// "features":[{"type":"Feature","properties":{"mag":1.67,"place":"3km E of The Geysers, CA",
-// "time":1656342942120,
-// "updated":1656343933419,
-// "tz":null,
-// "url":"https://earthquake.usgs.gov/earthquakes/eventpage/nc73751661",
-// "detail":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73751661.geojson",
-// "felt":null,
-// "cdi":null,
-// "mmi":null,
-// "alert":null,
-// "status":"automatic",
-// "tsunami":0,"sig":43,
-// "net":"nc","code":"73751661",
-// "ids":",nc73751661,",
-// "sources":",nc,",
-// "types":",nearby-cities,origin,phase-data,scitech-link,",
-// "nst":29,"dmin":0.007542,"rms":0.04,"gap":78,"magType":"md",
-// "type":"earthquake","title":"M 1.7 - 3km E of The Geysers, CA"},
-// "geometry":{"type":"Point",
-// "coordinates":[-122.7273331,38.7815018,1.31]},
-// "id":"nc73751661"},
